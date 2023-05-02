90d7290716e3c9729800e440fd314fd8c4629e6d
strike1031 <Strike10310522@gmail.com>
2023-05-02 12:55:54 -0700

    fix prettier issues in listeners.js
    fix prettier issues in listeners.js

--- src/pages/Main/listeners.js
+++ src/pages/Main/listeners.js
@@ -1,7 +1,7 @@
 export function dragMoveListener(event) {
     if (event.target === null) return { x: 0, y: 0 };
-    //const target = event.target as HTMLDivElement;
-    const target = event.target;
+    // const target = event.target as HTMLDivElement;
+    const {target} = event;
     // keep the dragged position in the data-x/data-y attributes
     const x =
         // @ts-expect-error - no type
@@ -11,7 +11,7 @@ export function dragMoveListener(event) {
         (parseFloat(target.getAttribute("data-y") ?? "0") || 0) + event.dy;
 
     // translate the element
-    target.style.transform = "translate(" + x + "px, " + y + "px)";
+    target.style.transform = `translate( ${x}px, ${y}px )`;
 
     // update the posiion attributes
     target.setAttribute("data-x", x);
@@ -21,19 +21,19 @@ export function dragMoveListener(event) {
 }
 
 export function resizeListener(event) {
-    const target = event.target;
+    const {target} = event;
     let x = parseFloat(target.getAttribute("data-x")) || 0;
     let y = parseFloat(target.getAttribute("data-y")) || 0;
 
     // update the element's style
-    target.style.width = event.rect.width + "px";
-    target.style.height = event.rect.height + "px";
+    target.style.width = `${event.rect.width}px`;
+    target.style.height = `${event.rect.height}px`;
 
     // translate when resizing from top or left edges
     x += event.deltaRect.left;
     y += event.deltaRect.top;
 
-    target.style.transform = "translate(" + x + "px," + y + "px)";
+    target.style.transform = `translate( ${x} px, ${y} px )`;
 
     target.setAttribute("data-x", x);
     target.setAttribute("data-y", y);
